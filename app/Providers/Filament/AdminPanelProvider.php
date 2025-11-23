8d57e131f888037237a092e76d9cef16c86fe8bb
jstalinko <alinkodotid@gmail.com>
2025-11-23 13:18:32 +0700

    “ Do what you can, with what you have, where you are. ”   — Theodore Roosevelt

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('dashboard')
-            ->registration()
+            ->profile()
             ->login()
             ->colors([
                 'primary' => Color::Cyan,
